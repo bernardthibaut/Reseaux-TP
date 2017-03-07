998342bb77d9ab6647bc66f91944ecbbdaedffdb
BERNARD Thibaut <thibaut1.bernard@etudiant.univ-lille1.fr>
2017-03-07 17:11:05 +0100

    TP3 : Q1 & Q2

--- TP3/c/sendUDP.c
+++ TP3/c/sendUDP.c
@@ -11,22 +11,33 @@
 #include <stdio.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <string.h>
 
-
-const char* ADDRESS = "244.0.0.1";
+const char* ADDRESS = "127.0.0.1";
 const short PORT = 7654;
 
 int main(int argc, char const *argv[]) {
     struct sockaddr_in add;
     int s;
+    const char *buf = argv[1];
+
+    if(argc != 2){
+        perror("Wrong usage: sendUDP [msgToSend]");
+        return EXIT_FAILURE;
+    }
 
     add.sin_family = AF_INET;
-    add.sin_port = PORT;
+    add.sin_port = htons(PORT);
     add.sin_addr.s_addr = inet_addr(ADDRESS);
 
-    s = socket(AF_INET, SOCK_STREAM, 0);
+    s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if(s == -1){
         perror("Error socket");
+        return EXIT_FAILURE;
+    }
+
+    if(sendto(s, buf, strlen(buf), 0, (struct sockaddr *) &add, sizeof(add))  < 0){
+        perror("Error send");
         return EXIT_FAILURE;
     }
 
